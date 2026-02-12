58a8575e33e069aabe161a284ac578d2c5d84326
tofikvel <tofig.valiyev1993@gmail.com>
2026-02-12 21:25:30 +0100

    added basic functionality to the product screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -12,7 +12,7 @@ const ProductScreen = () => {
 
     return (
         <>
-            <Link className='btn btn-light my-3 to="/'>Go back</Link>
+            <Link className='btn btn-light my-3' to='/'>Go back</Link>
             <Row>
                 <Col md={5}>
                     <Image src={product.image} alt={product.name} fluid />
@@ -26,6 +26,7 @@ const ProductScreen = () => {
                             <Rating value={product.rating} text={product.numReviews} />
                         </ListGroup.Item>
                         <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                        <ListGroup.Item>{product.description}</ListGroup.Item>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
@@ -36,8 +37,21 @@ const ProductScreen = () => {
                                     <Col>
                                         Price:
                                     </Col>
-                                    <Col>${product.price}</Col>
+                                    <Col><strong>${product.price}</strong></Col>
                                 </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</strong></Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                                    Add To Cart
+                                </Button>
                             </ListGroup.Item>
                         </ListGroup>
                     </Card>
